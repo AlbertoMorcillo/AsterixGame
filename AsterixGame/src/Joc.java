1bed3b3d9c0709b5e394721bcdfc24f23d703cd9
AlbeX <a.morcillo@sapalomera.cat>
2023-05-11 09:05:27 +0200

    Añado las clases Joc.java y  Taulell.java

---
+++ AsterixGame/src/Joc.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Joc {
+    private static jugador asterix;
+    private static taulell bosc;
+    static int COLUMNES;
+    static int FILES;
+    private static boolean fi;
+    private static Enemic[] legionaris;
+    static Scanner scan = new Scanner(System.in);
+
+    public Joc() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+    public static boolean mouEnemics(){
+        return true;
+    }
+    public static boolean mouJugador(){
+        return true;
+    }
+    public static Coordenades posicioAleatoria(){
+
+        return Coordenades;
+    }
+}
---
+++ AsterixGame/src/Taulell.java
@@ -0,0 +1,16 @@
+public class Taulell {
+    private int columnes;
+    private int files;
+    private static char[][] taulell;
+
+    public Taulell(int files, int columnes){
+        this.files = files;
+        this.columnes = columnes;
+    }
+    public void dibu(){
+
+    }
+    public void (Pocio p, Jugador, Enemic[] enemics){
+
+    }
+}
