8ac1d4ee682148d5bb37323796f9af5213641701
AlbeX <a.morcillo@sapalomera.cat>
2023-05-12 10:02:33 +0200

    Añado las clases Joc.java y  Taulell.java

--- AsterixGame/src/Taulell.java
+++ AsterixGame/src/Taulell.java
@@ -9,9 +9,11 @@ public class Taulell {
     }
     public void dibu(){
         taulell = new char[files][columnes];
-
-
-
+        for (int i = 0; i < files; i++) {
+            for (int j = 0; j < columnes; j++) {
+                System.out.print(" " + taulell[i][j] + " ");
+            }
+        }
     }
     public void init (Pocio p, Jugador jug, Enemic[] enemics){
 
